e96ffb3c2112a8a0d5941e7fb25cef8d92e3a5f0
Nikolai Lønberg <niklonberg@gmail.com>
2023-12-21 19:00:03 +0100

    add saveCSV method, install appendFileSync

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,6 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = require("fs");
 class CSVWriter {
     constructor(columns) {
         this.columns = columns;
@@ -6,6 +8,11 @@ class CSVWriter {
     }
     formatRow(p) {
         return this.columns.map((col) => p[col]).join(",");
+    }
+    saveCSV(filename) {
+        (0, fs_1.appendFileSync)(filename, this.csv);
+        this.csv = "\n";
+        console.log("file saved to: ", filename);
     }
     addRows(payments) {
         let rows = payments.map((v) => this.formatRow(v));
@@ -18,3 +25,4 @@ writer.addRows([
     { to: "mario", from: "luigi", amount: 500, service: "web dev work" },
     { to: "peach", from: "toad", amount: 100, service: "cake" },
 ]);
+writer.saveCSV("./data/payments.csv");
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { appendFileSync } from "fs";
+
 interface Payment {
   to: string;
   from: string;
@@ -18,6 +20,13 @@ class CSVWriter {
     return this.columns.map((col) => p[col]).join(",");
   }
 
+  saveCSV(filename: string): void {
+    appendFileSync(filename, this.csv);
+    this.csv = "\n";
+
+    console.log("file saved to: ", filename);
+  }
+
   addRows(payments: Payment[]): void {
     let rows = payments.map((v) => this.formatRow(v));
 
@@ -28,7 +37,10 @@ class CSVWriter {
 }
 
 const writer = new CSVWriter(["to", "from", "amount", "service"]);
+
 writer.addRows([
   { to: "mario", from: "luigi", amount: 500, service: "web dev work" },
   { to: "peach", from: "toad", amount: 100, service: "cake" },
 ]);
+
+writer.saveCSV("./data/payments.csv");
